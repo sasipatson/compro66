0308bca6ac6ae36e0cd45f9d064b6c83a111e2e2
sasipatson <139094335+sasipatson@users.noreply.github.com>
2024-07-10 19:02:24 +0700

    Add files via upload

---
+++ HW_02.py
@@ -0,0 +1,11 @@
+N1 = int (input("Enter Integer 1 : "))
+N2 = int (input("Enter Integer 2 : "))
+N3 = int (input("Enter Integer 3 : "))
+
+Total = N1 + N2 + N3
+
+print ("Value Number : ", N1)
+print ("Value Number : ", N2)
+print ("Value Number : ", N3)
+
+print("Total All : ", Total)
---
+++ HW_03.py
@@ -0,0 +1,11 @@
+N1 = float (input("Enter Float Number 1 : "))
+N2 = float (input("Enter Float Number 2 : "))
+N3 = float (input("Enter Float Number 3 : "))
+
+Total = N1 + N2 + N3
+
+print ("Value Number : ", N1)
+print ("Value Number : ", N2)
+print ("Value Number : ", N3)
+
+print("Total All : ", Total)
